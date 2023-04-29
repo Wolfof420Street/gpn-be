4dec8d16902e0d5ffe09728bf4d19348e61c6fd3
wolf <billodida420@gmail.com>
2023-04-29 04:27:56 +0300

    changing folder

--- src/app/main.ts
+++ src/app/main.ts
@@ -14,4 +14,6 @@ const server = new ApolloServer({
 
 server.listen({ port: process.env.PORT || 3000 }).then(({ url }) => {
     console.log(`graphql api running at ${url}graphql`);
+    console.log(`Server ready at ${url}`);
+    console.log(`Listening on port ${process.env.PORT || 3000}`);
 });
